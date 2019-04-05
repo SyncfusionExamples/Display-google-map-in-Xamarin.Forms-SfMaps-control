4a327c7a0fa54c979e65f57d203d366add90a0f3
ROOT\deviaruna.murugan <deviaruna.murugan@syncfusion.com>
2019-04-05 15:26:44 +0530

    Sample using RequestTileUri has committed

---
+++ Google_map/Google_map/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Google_map
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This event is used to pass the uri of desired maps tile provider
+        /// </summary>
+        /// <param name="sender">The imagery layer</param>
+        /// <param name="e">The Tile uri arguments</param>
+        private void ImageryLayer_RequestTileUri(object sender, Syncfusion.SfMaps.XForms.TileUriArgs e)
+        {
+            var link = "http://mt1.google.com/vt/lyrs=y&x=" + e.X.ToString() + "&y=" + e.Y.ToString() + "&z=" + e.ZoomLevel.ToString();
+            e.Uri = link;
+           
+        }
+    }
+}
